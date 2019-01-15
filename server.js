ae9c03f489cdee77ce0cd484e60ed8133fe19264
Shaba9 <shaba92@gmail.com>
2019-01-15 15:37:25 -0800

    tester passes test

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const http = require('http');
+const app = require('./lib/app');
 const PORT = 7890;
 
-http.createServer((req, res) => {
-  console.log('REQUEST INCOMING!');
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html><body><h1>THANKS FOR VISITING!</h1>');
-  res.end('Bye!</body></html>');
-}).listen(PORT, () => console.log('SERVER RUNNING'));
-
-
+http.createServer(app)
+  .listen(PORT);
